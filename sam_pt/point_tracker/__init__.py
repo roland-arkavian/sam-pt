d4755242ed7706894e0d882587c67564be6cbbf1
Roland Li <roland@arkavian.com>
2024-07-29 13:46:05 -0700

    only import used trackers

--- sam_pt/point_tracker/__init__.py
+++ sam_pt/point_tracker/__init__.py
@@ -1,7 +1,3 @@
 from .tracker import PointTracker
 from .pips import PipsPointTracker
-from .raft import RaftPointTracker
-from .superglue import SuperGluePointTracker
 from .tapir import TapirPointTracker
-from .tapnet import TapnetPointTracker
-from .cotracker import CoTrackerPointTracker
